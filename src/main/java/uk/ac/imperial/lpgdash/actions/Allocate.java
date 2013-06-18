c697f540d64cbaad9b3573d4013c7f125b65b8f0
Sam Macbeth <sm1106@imperial.ac.uk>
2013-06-18 13:59:37 +0100

    Random order appropriating.

--- src/main/java/uk/ac/imperial/lpgdash/actions/Allocate.java
+++ src/main/java/uk/ac/imperial/lpgdash/actions/Allocate.java
@@ -1,16 +1,24 @@
 package uk.ac.imperial.lpgdash.actions;
 
 import uk.ac.imperial.lpgdash.facts.Player;
+import uk.ac.imperial.presage2.core.util.random.Random;
 
 public class Allocate extends TimestampedAction {
 
 	Player player;
 	double quantity;
+	double order = 0;
 
 	public Allocate(Player p, double quantity, int time) {
 		super(time);
 		this.player = p;
 		this.quantity = quantity;
+		this.order = Random.randomDouble();
+	}
+
+	public Allocate(Player p, double quantity, int time, double order) {
+		this(p, quantity, time);
+		this.order = order;
 	}
 
 	public Player getPlayer() {
@@ -23,8 +31,8 @@ public class Allocate extends TimestampedAction {
 
 	@Override
 	public String toString() {
-		return "Allocate [player=" + player.getName() + ", quantity=" + quantity + ", t="
-				+ t + "]";
+		return "Allocate [player=" + player.getName() + ", quantity="
+				+ quantity + ", t=" + t + "]";
 	}
 
 }
